41ffe679df71a441b5abd5f1aeb22e146237f10f
Marcela Oliveira <marcela.oliveira244@gmail.com>
2021-08-11 14:58:36 -0300

    first commit

---
+++ Documents/Workspace/primeiro-programa/src/br/com/dio/model/Gato.java
@@ -0,0 +1,26 @@
+package br.com.dio.model;
+
+/**
+ * @author Marcela Oliveira
+ *
+ */
+public class Gato {
+	
+	public String nome;
+	public String cor;
+	public Integer idade;
+	
+	public Gato() {
+		super();
+	}
+
+	public Gato(String nome, String cor, Integer idade) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+	
+	
+
+}
